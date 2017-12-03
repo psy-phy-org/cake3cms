d0754993f660399bfbcf87e1004df3d7233da554
psy-phy-owner <psyphyowner@gmail.com>
2017-12-03 10:23:04 +0900

    create articles controller

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+
+        // これは一時的なもので、後で認証を構築するときに
+        // 削除されます。
+        if (!$entity->user_id) {
+            $entity->user_id = 1;
+        }
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmpty('body')
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+}
